80b018e18d981b3d48d24c8864a3df6d9b488bd4
JesusPortillo <jesusdavidpv12@gmail.com>
2021-09-10 19:03:23 -0500

    Clase servicios persona creada y funcion de listar personas definida

--- src/main/java/com/sofkau/crudPerson/controllers/PersonController.java
+++ src/main/java/com/sofkau/crudPerson/controllers/PersonController.java
@@ -1,10 +1,23 @@
 package com.sofkau.crudPerson.controllers;
 
+import com.sofkau.crudPerson.entities.Person;
+import com.sofkau.crudPerson.services.PersonServicesInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("api")
 public class PersonController {
+
+    @Autowired
+    private PersonServicesInterface service;
+
+    @GetMapping(value = "listPersons")
+    public Iterable<Person>listPersons(){
+        return service.listAllPersons();
+    }
+
 
 }
---
+++ src/main/java/com/sofkau/crudPerson/services/PersonServices.java
@@ -0,0 +1,38 @@
+package com.sofkau.crudPerson.services;
+
+import com.sofkau.crudPerson.entities.Person;
+import com.sofkau.crudPerson.repositories.PersonInterfaceRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+
+public class PersonServices implements PersonServicesInterface{
+
+    @Autowired
+    private PersonInterfaceRepository data;
+
+    @Override
+    public ArrayList<Person> listAllPersons() {
+        return (ArrayList<Person>) data.findAll();
+    }
+
+    @Override
+    public Person listPersonById(int id) {
+        return null;
+    }
+
+    @Override
+    public Person savePerson(Person person) {
+        return null;
+    }
+
+    @Override
+    public void deletePerson(int id) {
+
+    }
+
+    @Override
+    public Person updatePerson(Person person) {
+        return null;
+    }
+}
